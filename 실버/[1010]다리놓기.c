fca3a0962c457d03fe6fdf2ee9a649d60740cb95
jiwon <jidole041214@naver.com>
2021-07-07 08:16:45 +0900

    .

--- 실버/[1010]다리놓기.c
+++ 실버/[1010]다리놓기.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 #define _CRT_SECURE_NO_WARNINGS
 #pragma warning(disable: 4996)
 
-int factorial(int n)
-{
-	if (n == 1)
-		return 1;
-
-	return n * factorial(n - 1);
-}
-
-int nCr(int y, int x) {
-	return factorial(x) / (factorial(y - x) * factorial(x));
-}
+int arr[10000] = { 0 };
 
 int main() {
-	int test_num = 0;
-	int x, y = 0;
-	//scanf("%d", &test_num);
-	scanf("%d %d", &x, &y);
-	printf("\n%d", comb(y, x));
+    int test_num = 0;
+    int n, r = 0;
+    scanf("%d", &test_num);
+    for (int i = 0; i < test_num; i++) {
+        long double nf = 1;
+        long double af = 1;
+        scanf("%d %d", &r, &n);
+        if (r == n) {
+            arr[i] = 1;
+            continue;
+        }
+        if (r > n - r) r = n - r;
+        for (int j = n; j > r; j--) nf *= j;
+        for (int j = 1; j <= n - r; j++) af *= j;
+        arr[i] = (int)(round(nf / af));
+    }
+    for (int i = 0; i < test_num; i++) printf("%d\n", arr[i]);
+    return 0;
 }
