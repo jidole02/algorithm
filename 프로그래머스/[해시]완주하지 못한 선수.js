640f830ba196bc84365327df6c82b25a09c94aee
jiwon <jidole041214@naver.com>
2021-09-01 21:33:15 +0900

    .

---
+++ 프로그래머스/[해시]완주하지 못한 선수.js
@@ -0,0 +1,9 @@
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) return participant[i];
+  }
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/42576?language=javascript
